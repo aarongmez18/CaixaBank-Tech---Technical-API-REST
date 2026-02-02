4bbfd6184281731ce1226d59ae3a768057eb995c
aarongmez18 <aarongmez18@gmail.com>
2026-02-02 20:45:24 +0100

    Initial commit

---
+++ src/main/java/api/caixaTech/CaixaTechApplication.java
@@ -0,0 +1,13 @@
+package api.caixaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/caixaTech/CaixaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package api.caixaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
