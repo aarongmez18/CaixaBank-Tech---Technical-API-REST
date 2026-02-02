c9691087d4642cef2f70420fdc001c69cf08ffe0
aarongmez18 <aarongmez18@gmail.com>
2026-02-02 21:02:41 +0100

    Status enum created

--- src/main/java/api/caixaTech/model/enums/Status.java
+++ src/main/java/api/caixaTech/model/enums/Status.java
@@ -1,4 +1,8 @@
 package api.caixaTech.model.enums;
 
 public enum Status {
+    PENDING, // Solicitud pendiente
+    APPROVED, // Solicitud aprobada
+    REJECTED, // Solicitud rechazada
+    CANCELLED // Solicitud cancelada
 }
